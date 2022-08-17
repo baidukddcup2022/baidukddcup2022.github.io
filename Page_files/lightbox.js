d162e45ad72a6e701034ea24911e5e4bb4c2d1f4
baidukddcup2022 <baidukddcup2022@outlook.com>
2022-08-17 20:48:32 +0800

    add paper information

---
+++ Page_files/lightbox.js
@@ -0,0 +1,12 @@
+$(function() {
+
+	
+	// if the function argument is given to overlay,
+	// it is assumed to be the onBeforeLoad event listener
+	$("a[rel]").overlay({
+		
+		mask: 'white',
+		opacity:0.8,
+		top:"center"
+	});
+});
